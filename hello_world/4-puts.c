5a9501b6a0fe89f7d480f3700808dbbbf9b27816
root <archabdulrm@gmial.com>
2024-02-16 08:12:18 +0000

    Hello, puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a message to the console
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	 /* Using puts to print */
+	puts("\"Programming is like building a multilingual puzzle");
+	/* returns 0 upon successful execution */
+	return (0);
+}
