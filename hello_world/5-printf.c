6a7f073321a62464799ab2c306599a8bf82ca155
root <archabdulrm@gmial.com>
2024-02-16 08:26:03 +0000

    Hello, printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a message to the console
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	 /* Using puts to print */
+	printf("%s\n","with proper grammar, but the outcome is a piece of art,");
+	/* returns 0 upon successful execution */
+	return (0);
+}
