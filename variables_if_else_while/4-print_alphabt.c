280a99fe21f36955ebd0fd450fae0186c5288ac5
amirasabdu <archabdulrm@gmail.com>
2024-02-17 07:53:34 +0000

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Print the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'e' || i == 'q')
+			continue;
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
