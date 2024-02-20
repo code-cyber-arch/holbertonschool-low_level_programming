e32882798e4a0f52b96779a2027dfe277bda2fbb
amirasabdu <archabdulrm@gmail.com>
2024-02-20 00:06:15 +0000

    98 Battery Street, the OG

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints numbers to 98 from the given number
+ * @n: The starting number
+ *
+ * Description: This function prints numbers from @n to 98, inclusive.
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		printf("%d", n);
+
+		for (i = n + 1; i >= 98; i--)
+		{
+			printf(", %d", i);
+		}
+	}
+	else if (n < 98)
+	{
+		printf("%d", n);
+
+		for (i = n + 1; i <= 98; i++)
+		{
+			printf(", %d", i);
+		}
+	}
+	else
+		printf("%d", n);
+	printf("\n");
+}
