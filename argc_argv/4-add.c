13a8c3ef41ef57496c88c5f2e239284991f69c3e
amirasabdu <archadulrm@gmail.com>
2024-03-08 06:02:52 +0000

    Sums posetive numbers

---
+++ argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Sums positive integers passed as command-line arguments.
+ * @argc: Number of arguments.
+ * @argv: Array of argument strings.
+ *
+ * Return: 0 on success, 1 on error.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (isdigit(argv[i][0]))
+		{
+			if (atoi(argv[i]) > 0)
+				sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
