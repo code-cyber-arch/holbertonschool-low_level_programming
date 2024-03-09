7ee019ae6eff5905b3343aa78d7b6029c2abcded
amirasabdu <archabdulrm@gmail.com>
2024-03-09 05:09:28 +0000

    If you even dream of beating me you'd better wake up and apologize

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		array[i] = malloc(width * sizeof(int));
+		array[i] = malloc(width * sizeof(int *));
 		if (array[i] == NULL)
 		{
 			for (; i >= 0; i--)
