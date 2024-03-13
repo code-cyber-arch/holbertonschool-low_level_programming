0625b3e9914a61794c17311111ddeaeb0394564c
amirasabdu <archabdulrm@gmail.com>
2024-03-13 03:04:22 +0000

    Main Header file

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -6,5 +6,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
 
 #endif
