c10a3bff0181417cf396718402300b4ee91096ff
amirasabdu <archabdulrm@gmail.com>
2024-04-06 23:28:22 +0000

    00

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -13,6 +13,8 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0, bit_position_value = 1;
 	int len = 0;
 
+	if (b == NULL)
+		return (0);
 	if (*b == '\0')
 		return (0);
 	while (b[len] != '\0')
