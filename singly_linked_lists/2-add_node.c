500687a93d183f4ca340b532bfab7aa3fc2c641c
amirasabdu <archabdulrm@gmail.com>
2024-04-06 05:03:50 +0000

    Add node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -13,17 +13,23 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new = malloc(sizeof(list_t));
+	char *dup;
+	int len;
+	list_t *new;
 
-	if (!new)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
-	new->str = strdup(str);
-	if (!(new->str))
+	dup = strdup(str);
+	if (dup == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
-	new->len = strlen(str);
+	for (len = 0; str[len];)
+		len++;
+	new->str = dup;
+	new->len = len;
 	new->next = *head;
 	*head = new;
 	return (new);
