6dc4e89a14d05485e573893b3f2b20090679831b
amirasabdu <archabdulrm@gmail.com>
2024-04-07 06:31:02 +0000

    Main Header with more function calls

--- file_io/main.h
+++ file_io/main.h
@@ -10,5 +10,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void handle_error(int src_fd, int dest_fd, char *src_file);
+void copy_file(const char *src_file_name, const char *dest_file_name);
 
 #endif
