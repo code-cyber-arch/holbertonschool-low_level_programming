7780e97bd5f2c9c59c9b4440c9c4adbaa3f1404c
amirasabdu <archabdulrm@gmail.com>
2024-04-07 07:41:53 +0000

    Main Header file update

--- file_io/main.h
+++ file_io/main.h
@@ -11,7 +11,8 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void handle_error(int src_fd, int dest_fd, const char *src_file);
-void copy_file(const char *src_file_name, const char *dest_file_name);
+void check_open_files(int src_fd, int dest_fd, char *argv[]);
+void copy_files(int src_fd, int dest_fd);
+void close_files(int src_fd, int dest_fd);
 
 #endif
